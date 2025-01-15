ad7c7721b74197beaadf10e0eb8ac2932c03b976
kjetil <kjrea003@osloskolen.no>
2025-01-15 14:20:04 +0100

    active item

--- My Mod/main.lua
+++ My Mod/main.lua
@@ -20,15 +20,37 @@ function mod:EvaluateCache(player, cacheFlags)
     if cacheFlags & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then
         local itemCount = player:GetCollectibleNum(test)
         local damageToAdd = damagePotionDamage * itemCount
-        player.Damage = player.Damage -damageToAdd
+        player.Damage = player.Damage +damageToAdd
         
     end
     if cacheFlags & CacheFlag.CACHE_FIREDELAY==CacheFlag.CACHE_FIREDELAY then
         local itemCount = player:GetCollectibleNum(test)
         local thing=tearss * itemCount
-        player.MaxFireDelay = player.MaxFireDelay + thing
+        player.MaxFireDelay = player.MaxFireDelay - thing
 
     end
 end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
+
+
+local eksempe = Isaac.GetItemIdByName("eksempel")
+
+function mod:ekssempel()
+    local entities = Isaac.GetRoomEntities();
+    for _, entity in ipairs(entities) do
+        if entity:IsActiveEnemy()then
+            entity:Kill()
+        end
+    end
+
+    return {
+        Discharge = true,
+        Remove = false,
+    }
+end
+function set()
+    local player =Isaac.GetPlayer()
+    player:NeedsCharge(ActiveSlot.SLOT_PRIMARY)
+end
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.ekssempel, eksempe)
